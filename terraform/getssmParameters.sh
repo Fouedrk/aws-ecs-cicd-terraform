87de193fed81298f602933c032739ac44b8ad57e
Foued Rekik <62577805+Fouedrk@users.noreply.github.com>
2023-07-03 12:14:06 +0100

    Update getssmParameters.sh

--- terraform/getssmParameters.sh
+++ terraform/getssmParameters.sh
@@ -1,4 +1,4 @@
-aws ssm get-parameter --name "/database/GameDay/endpoint"  --region eu-west3 --with-decryption
-aws ssm get-parameter --name "/database/GameDay/name"  --region eu-west3 --with-decryption
-aws ssm get-parameter --name "/database/GameDay/password"  --region eu-west3 --with-decryption
-aws ssm get-parameter --name "/database/GameDay/user"  --region eu-west3 --with-decryption
+aws ssm get-parameter --name "/database/GameDay/endpoint"  --region eu-west-3 --with-decryption
+aws ssm get-parameter --name "/database/GameDay/name"  --region eu-west-3 --with-decryption
+aws ssm get-parameter --name "/database/GameDay/password"  --region eu-west-3 --with-decryption
+aws ssm get-parameter --name "/database/GameDay/user"  --region eu-west-3 --with-decryption
